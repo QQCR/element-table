0d119e7f9ff65762bca76702213bf41dc9130402
qqcr <378626360@qq.com>
2020-06-16 10:28:07 +0800

    tag only

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,82 +1,35 @@
 <template>
   <div>
-
     <table-page
-      rowKey="id"
-      :list="list"
-      :total="total"
+      :list="listData"
       :table_title="table_title"
-      :operates="operates"
-      :pageObj="pageObj"
-      :show_pagination="show_pagination"
-      @onPageChange="pageChange"
-      @onSizeChange="sizeChange"
-      @editChange="editChange"
     >
       <template v-slot:tags="scope">
         <el-tag
-          v-if="scope.scope.row.tag == 1"
+          v-if="scope.scope.tag == 1"
           size="small"
           type="primary"
           >tag1
         </el-tag>
         <el-tag
-          v-else-if="scope.scope.row.tag == 2"
+          v-else-if="scope.scope.tag == 2"
           size="small"
           type="warning"
           >tag2
         </el-tag>
         <el-tag
-          v-else-if="scope.scope.row.tag == 3"
+          v-else-if="scope.scope.tag == 3"
           size="small"
           type="success"
           >tag3
         </el-tag>
       </template>
-      <template v-slot:operates="scope">
-        <table-operation 
-          :operations="operations"
-          :rawData="scope.scope.row"
-          @handleOperation="handleOperation"
-        ></table-operation>
-      </template>
     </table-page>
-    <el-dialog title="编辑" :visible.sync="editDialog">
-      <el-form :model="editInfo" label-position="left">
-        <el-form-item label="日期" :label-width="formLabelWidth">
-          <el-input v-model="editInfo.date"></el-input>
-        </el-form-item>
-        <el-form-item label="姓名" :label-width="formLabelWidth">
-          <el-input v-model="editInfo.name"></el-input>
-        </el-form-item>
-        <el-form-item label="省份" :label-width="formLabelWidth">
-          <el-input v-model="editInfo.province"></el-input>
-        </el-form-item>
-        <el-form-item label="市区" :label-width="formLabelWidth">
-          <el-input v-model="editInfo.city"></el-input>
-        </el-form-item>
-        <el-form-item label="地址" :label-width="formLabelWidth">
-          <el-input v-model="editInfo.address"></el-input>
-        </el-form-item>
-        <el-form-item label="状态" :label-width="formLabelWidth">
-          <el-select v-model="editInfo.tag" placeholder="请选择tag" style="display: block;">
-            <el-option label="tag1" value="1"></el-option>
-            <el-option label="tag2" value="2"></el-option>
-            <el-option label="tag3" value="3"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="closeDialog">取 消</el-button>
-        <el-button type="primary" @click="saveDialog">确 定</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import TablePage from '@/components/TablePage'
-import TableOperation from '@/components/TableOperation'
 
 const table_title = [
   {
@@ -139,26 +92,10 @@ const table_title = [
   },
 ];
 
-const operates = {
-  operate: true,
-  label: '操作',
-  width: '120px',
-  titleAlign: 'center',
-  columnAlign: 'center',
-};
-
-const operations = [
-  {
-    type: 'edit',
-    title: '编辑'
-  },
-];
-
 
 export default {
   components: {
     TablePage,
-    TableOperation
   },
   data() {
     return {
@@ -255,75 +192,9 @@ export default {
         }
       ],
       table_title: table_title,
-      total: 10,
-      operates: operates,
-      show_pagination:true,
-      operations:operations,
-      pageObj: {
-        page: 1,
-        pageSize: 2,
-        pageSizeList: [2, 4, 6],
-      },
-      editDialog: false,
-      editInfo: {
-        id: 0,
-        date: '',
-        name: '',
-        province: '',
-        city: '',
-        address: '',
-        tag: 0
-      },
-      formLabelWidth: '100px'
     }
   },
-  computed: {
-    list() {
-      let array = []
-      this.listData.forEach((item,index) => {
-        let i = Math.floor(index / this.pageObj.pageSize)
-        if(!array[i]) {
-           array.push([])
-        }
-        array[i].push(item)
-      })
-      return array[this.pageObj.page - 1]
-    }
-  },
-  methods: {
-    pageChange(page) {
-      this.pageObj.page = page
-    },
-    sizeChange(size) {
-      this.pageObj.pageSize = size
-      this.pageObj.page = 1
-    },
-    handleOperation(op,row) {
-      this.editDialog = true
-      console.log(op,row)
-      this.editInfo.id = row.id
-      this.editInfo.date = row.date
-      this.editInfo.name = row.name
-      this.editInfo.province = row.province
-      this.editInfo.city = row.city
-      this.editInfo.address = row.address
-      this.editInfo.tag = row.tag
-    },
-    closeDialog() {
-      this.editDialog = false
-    },
-    saveDialog() {
-      this.editDialog = false
-      console.log(this.editInfo)
-    },
-    editChange(oldV, newV, row, name) {
-      this.listData.forEach(item => {
-        if(item.id === row.id) {
-          item[name] = newV
-        }
-      })
-    }
-  }
+   
 }
 </script>
 
--- src/components/TablePage/index.vue
+++ src/components/TablePage/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="table-content">
-
     <el-table
       :data="list"
       class="mt-10"
@@ -10,7 +9,6 @@
       stripe
       empty-text="暂无数据"
       :highlight-current-row="true"
-      :row-key="handleReserve"
     >
       <el-table-column
         v-for="(item, index) in table_title"
@@ -24,66 +22,20 @@
         :header-align="item.titleAlign"
       >
         <template slot-scope="scope">
-          <el-image
-            v-if="item.prop === 'thumb' && scope.row.thumb.length > 0"
-            :src="decodeURI(scope.row.thumb[0])"
-            :preview-src-list="[scope.row.thumb[0]]"
-            :lazy="true"
-            fit="cover"
-            style="width:50px;height:40px;"
-          ></el-image>
-          <template v-else-if="item.tag">
-            <slot name="tags" :scope="scope" :column="item.prop"></slot>
+          <template v-if="item.tag">
+            <slot name="tags" :scope="scope.row"></slot>
           </template>
-          <span
-            v-else-if="!item.isEdit"
-          >{{ getText(scope.row, item.prop)}}</span>
-          <!-- <el-input v-else-if="textarea[item.prop]" v-model="scope.row[item.prop]" type="textarea"></el-input> -->
-          <editable-cell v-else v-model="scope.row[item.prop]" @output="changeCell" :row="scope.row" :name="item.prop">
-            <span slot="content">{{scope.row[item.prop]}}</span>
-          </editable-cell>
-          <!-- <el-input v-else v-model="scope.row[item.prop]"></el-input> -->
-        </template>
-      </el-table-column>
-      <!-- 如果需要自定义最后一栏则需传入operates对象,并提供模板 -->
-      <el-table-column v-if="operates.operate" 
-      :label="operates.label" 
-      :width="operates.width"
-      :align="operates.columnAlign"
-      :header-align="operates.titleAlign">
-        <template slot-scope="scope">
-          <slot name="operates" :scope="scope"></slot>
+          <span v-else>{{scope.row[item.prop]}}</span>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-      v-show="show_pagination"
-      :current-page.sync="page"
-      :page-sizes="page_list"
-      :page-size="page_size"
-      :total="this.total"
-      layout="total,sizes, prev, pager, next, jumper"
-      class="table-pagination mt-10"
-      @current-change="onPageChange"
-      @size-change="onSizeChange"
-    />
   </div>
 </template>
 
 <script>
-import EditableCell from '@/components/EditableCell';
-
 export default {
   name: 'TablePage',
-  components: {
-    EditableCell
-  },
   props: {
-    show_pagination: {
-      //是否显示分页栏
-      type: Boolean,
-      default: () => true
-    },
     table_title: {
       //table的标题栏
       type: Array,
@@ -94,23 +46,9 @@ export default {
         }
       ]
     },
-    operates: {
-      //是否增加对表行的操作功能
-      type: Object,
-      default: () => {
-        return {
-          operate: false,
-          label: ''
-        };
-      }
-    },
     list: {
       type: Array,
       default: () => null
-    },
-    total: {
-      type: Number,
-      default: () => 0
     },
     title: {
       type: Array,
@@ -120,59 +58,10 @@ export default {
       type: String,
       default: () => ''
     },
-    pageObj: {
-      type: Object,
-      default: () => {
-        return {
-          page: 1,
-          pageSize: 25,
-          pageSizeList: [25, 50, 100]
-        };
-      }
-    }
-  },
-  data() {
-    return {
-      page: 0, //分页栏当前页面
-      page_size: 0, //分页栏大小,
-      page_list: [],
-      listQuery: {
-        title: undefined,
-        type: undefined
-      },
-      showColumns: []
-    };
-  },
-  created() {
-    this.page = this.pageObj.page;
-    this.page_size = this.pageObj.pageSize;
-    this.page_list = this.pageObj.pageSizeList;
-  },
-  watch: {
-    pageObj: {
-      handler: function(val) {
-        this.page = val.page;
-        this.page_size = val.pageSize;
-      },
-      deep: true
-    }
   },
   methods: {
-    onPageChange(page) {
-      this.$emit('onPageChange', page);
-    },
-    onSizeChange(pagesize) {
-      this.$emit('onSizeChange', pagesize);
-    },
     getText(row, key) {
       return row[key];
-    },
-    changeCell(oldVal, newVal, row, name) {
-      this.$emit('editChange',oldVal, newVal, row, name);
-    },
-    handleReserve(row) {
-      let id = this.rowkey;
-      return row[id];
     },
   }
 };
